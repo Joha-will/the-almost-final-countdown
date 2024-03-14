bed665f20cf121109297cab448f143c27fe839bd
Joha Wilson <johawilsonatu@icloud.com>
2024-03-14 14:51:38 +0000

    Used state to manage user input in Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("")
+  const [submittedName, setSubmittedName] = useState(false)
+
+  function handleNameChange(e) {
+    setSubmittedName(false)
+    setPlayerName(e.target.value)
+  }
+
+  function handleClick() {
+    setSubmittedName(true)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submittedName ? playerName : "Unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
