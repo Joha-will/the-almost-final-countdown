50b5a4dbc8904e6d1fe7c3beeda1b2e097c1430a
Joha-will <johawilsonatu@icloud.com>
2024-03-19 22:40:32 +0000

    updated handleClick funciton and ternary expression in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,18 +2,18 @@ import { useState, useRef } from "react";
 
 export default function Player() {
   const playerNameInput = useRef()
-  const [playerName, setPlayerName] = useState("")
+  const [playerName, setPlayerName] = useState(null)
 
 
   function handleClick() {
-    setSubmittedName(playerNameInput.current.value)
+    setPlayerName(playerNameInput.current.value)
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submittedName ? playerName : "Unknown entity"}</h2>
+      <h2>Welcome {playerName ?? "Unknown entity"}</h2>
       <p>
-        <input ref={playerName} type="text"/>
+        <input ref={playerNameInput} type="text"/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
